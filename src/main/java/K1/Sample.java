536b91e6215bae18bad48b9c0d32b31fad8313dd
TatsuyaKITABORI <45zombie45@gmail.com>
2019-05-14 16:40:09 +0900

    initial cimmit

---
+++ src/main/java/K1/Sample.java
@@ -0,0 +1,7 @@
+package K1;
+
+class Sample {
+  public static int add(int a, int b) {
+    return a + b;
+  }
+}
---
+++ src/test/resources/Test.java
@@ -0,0 +1,17 @@
+package java_test.resources;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HogeTest {
+  @Test
+  public void testAdd() {
+    assertEquals(3, 3);
+  }
+
+  @Test
+  public void testEqual() {
+    assertEquals("test", "test");
+  }
+
+}
